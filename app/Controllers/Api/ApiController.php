fc994560338e12ffe996470c418bb06b7eef7ce1
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2024-07-07 14:46:22 +0200

    add model

---
+++ app/Controllers/Api/ApiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\RESTful\ResourceController;
+use App\Model\StudentModel;
+
+class ApiController extends ResourceController
+{
+    // POST
+    public function addStudent()
+    {
+        // formData
+    }
+
+    // GET
+    public function listStudents()
+    {
+
+    }
+
+    // GET
+    public function singleStudent($student_id)
+    {
+
+    }
+
+    // PUT
+    public function updateStudent($student_id, $student_data)
+    {
+        // formData
+    }
+
+    // Delete
+    public function deleteStudent($student_id)
+    {
+
+    }
+}
