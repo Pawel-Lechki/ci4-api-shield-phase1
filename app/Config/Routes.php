40b40e8f3265ca11fa906e1c8bf983cbc6e4e9f7
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2024-07-07 14:52:49 +0200

    Add routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,15 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+// API routes
+$routes->group("api", ["namespace" => "App\Controllers\Api"], function ($routes) {
+
+  $routes->post('add-student', 'ApiController::addStudent');
+  $routes->get('list-students', 'ApiController::listStudent');
+  $routes->get("single-studnet/(:num)", 'ApiController::singleStudent/$1');
+  $routes->put("update-student/(:num)", 'ApiController::updateStudent/$1');
+  $routes->delete("delete-student/(:num)", 'ApiController::deleteStudent/$1');
+
+})
